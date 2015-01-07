0410b4ffe1190515d23d4a3c83fe9419f7079fee
Marcin Długajczyk <dlugajczykmarcin@gmail.com>
2015-01-07 19:39:30 +0000

    Implements tokenizer based on regular expressions.

---
+++ pyscm/tokenizer.py
@@ -0,0 +1,43 @@
+import re
+
+
+class Tokenizer(object):
+    def __init__(self, source):
+        extended_alphabetic_character = r"!|\$|%|&|\*|\+|-|\.|/|:|<|=|>|\?|@|\^|_|~"  # as per R5RS
+        rules = [("\(", "LPAREN"),
+                 ("\)", "RPAREN"),
+                 ("-??((\d+\.\d*)|(\d+.e-??\d+)|\d)+", 'NUMBER'),
+                 ('#', 'HASH'),
+                 ("({0}|[a-zA-Z]|\d)+".format(extended_alphabetic_character), 'SYMBOL')]
+        self._rules = [(re.compile(regex), name) for regex, name in rules]
+        self._source = source.strip()
+        self._position = 0
+        self._whitespace_regex = re.compile("\s+")
+
+    def tokenize(self):
+        tokens = []
+        while self._has_input():
+            tokens.append(self._next_token())
+        return tokens
+
+    def _next_token(self):
+        self._skip_whitespace()
+        for regex, name in self._rules:
+            result = regex.match(self._source[self._position:])
+            if result:
+                token = self._consume_token(result)
+                return token, name
+        raise Exception("Error in input stream at position %s" % self._position)
+
+    def _consume_token(self, match_result):
+        token = self._source[self._position:self._position + match_result.end()]
+        self._position += match_result.end()
+        return token
+
+    def _skip_whitespace(self):
+        result = self._whitespace_regex.match(self._source[self._position:])
+        if result:
+            self._consume_token(result)
+
+    def _has_input(self):
+        return self._position < len(self._source)
---
+++ test/tokenizer_test.py
@@ -0,0 +1,31 @@
+from pyscm.tokenizer import Tokenizer
+
+__author__ = 'Marcin Dlugajczyk'
+
+import unittest
+
+
+class TokenizerTest(unittest.TestCase):
+
+    def test_tokenizes_empty_input(self):
+        self.assertEqual(Tokenizer("   ").tokenize(), [])
+
+    def test_tokenizes_symbols(self):
+        self.assertEqual(Tokenizer(" -asdf*   foo-!@$bar").tokenize(), [("-asdf*", "SYMBOL"),
+                                                                             ("foo-!@$bar", "SYMBOL")])
+
+    def test_tokenizes_parenthesis(self):
+        self.assertEqual(Tokenizer(" ( foo (bar   )   fnord )").tokenize(),
+                         [("(", "LPAREN"), ("foo", "SYMBOL"), ("(", "LPAREN"),
+                          ("bar", "SYMBOL"), (")", "RPAREN"), ("fnord", "SYMBOL"), (")", "RPAREN")])
+
+    def test_tokenizes_numbers(self):
+        self.assertEqual(Tokenizer("-10  3.459 10e-3 10e34").tokenize(),
+                         [("-10", "NUMBER"), ("3.459", "NUMBER"), ("10e-3", "NUMBER"), ("10e34", "NUMBER")])
+
+    def test_tokenizes_hash_symbol(self):
+        self.assertEqual(Tokenizer("foo #(bar))").tokenize(),
+                         [("foo", "SYMBOL"), ("#", "HASH"), ("(", "LPAREN"),
+                          ("bar", "SYMBOL"), (")", "RPAREN"), (")", "RPAREN")])
+if __name__ == '__main__':
+    unittest.main()
