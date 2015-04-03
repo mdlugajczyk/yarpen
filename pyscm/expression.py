4b2540ab5e8b16ed4ea9e7aa15557888034c2548
Marcin Długajczyk <dlugajczykmarcin@gmail.com>
2015-04-03 20:37:02 +0100

    Pretty print expressions.

--- pyscm/expression.py
+++ pyscm/expression.py
@@ -2,15 +2,24 @@ class PyScmList(object):
     def __init__(self, exprs):
         self.expressions = exprs
 
+    def __repr__(self):
+        return self.expressions.__repr__()
+
 
 class PyScmQuoted(object):
     def __init__(self, datum):
         self.datum = datum
 
+    def __repr__(self):
+        return self.datum.__repr__()
+
 
 class PyScmNumber(object):
     def __init__(self, number):
         self.number = number
+
+    def __repr__(self):
+        return str(self.number)
 
 
 class PyScmSymbol(object):
@@ -20,7 +29,16 @@ class PyScmSymbol(object):
     def __eq__(self, other):
         return self.symbol == other.symbol
 
+    def __repr__(self):
+        return self.symbol
+
 
 class PyScmBoolean(object):
     def __init__(self, bool):
         self.bool = bool
+
+    def __repr__(self):
+        if self.bool:
+            return '#t'
+        else:
+            return '#f'
