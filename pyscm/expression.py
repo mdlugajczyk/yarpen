da03af1587162bf83e905d7b5bc22e933ab0926d
Marcin Długajczyk <dlugajczykmarcin@gmail.com>
2015-04-25 18:24:56 +0100

    Adds __eq__ to all expressions. Creates closure and free variable reference expressions

--- pyscm/expression.py
+++ pyscm/expression.py
@@ -2,6 +2,11 @@ class PyScmList(object):
     def __init__(self, exprs):
         self.expressions = exprs
 
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.expressions == other.expressions
+
     def __repr__(self):
         return self.expressions.__repr__()
 
@@ -10,6 +15,11 @@ class PyScmQuoted(object):
     def __init__(self, datum):
         self.datum = datum
 
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.datum == other.datum
+
     def __repr__(self):
         return self.datum.__repr__()
 
@@ -17,6 +27,11 @@ class PyScmQuoted(object):
 class PyScmNumber(object):
     def __init__(self, number):
         self.number = number
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.number == other.number
 
     def __repr__(self):
         return str(self.number)
@@ -39,8 +54,44 @@ class PyScmBoolean(object):
     def __init__(self, bool):
         self.bool = bool
 
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.bool == other.bool
+
     def __repr__(self):
         if self.bool:
             return '#t'
         else:
             return '#f'
+
+
+class PyScmClosure(object):
+    def __init__(self, body, free_variables, parameters):
+        self.body = body
+        self.free_variables = free_variables
+        self.parameters = parameters
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return (self.body == other.body
+                and self.free_variables == other.free_variables
+                and self.parameters == other.parameters)
+
+    def __repr__(self):
+        return "Closure: (%s) (%s) (%s)" % (self.body, self.free_variables,
+                                            self.parameters)
+
+
+class PyScmFreeVarRef(object):
+    def __init__(self, free_var):
+        self.free_var = free_var
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.free_var == other.free_var
+
+    def __repr__(self):
+        return "FreeVarReference %s" % self.free_var
