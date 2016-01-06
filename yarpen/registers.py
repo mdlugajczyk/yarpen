3680f078f2dfb3590334e0b63321cc11d2a16653
Marcin Długajczyk <dlugajczykmarcin@gmail.com>
2016-01-06 23:33:38 +0000

    Add new registers.

--- yarpen/registers.py
+++ yarpen/registers.py
@@ -1,8 +1,18 @@
 RAX = "%rax"
 RBX = "%rbx"
+RCX = "%rcx"
 RDX = "%rdx"
 RDI = "%rdi"
 RSP = "%rsp"
+RBP = "%rbp"
+R8 = "%r8"
+R9 = "%r9"
+R10 = "%r10"
+R11 = "%r11"
+R12 = "%r12"
+R13 = "%r13"
+R14 = "%r14"
+R15 = "%r15"
 AL = "%al"
 
 
